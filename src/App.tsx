460a8ac91d19bcfd18248742302225ab21127349
Fabio Sbergamo <fabzsbergamo@gmail.com>
2024-11-05 17:56:43 +0000

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <form>
+      <label> Value in GBP</label>
+      <input></input>
+      <label> Currency </label>
+      <select>
+        <option>
+
+        </option>
+      </select>
+
+      <button> Convert </button>
+    </form>
+    <div>
+      Result
+    </div>
+    </>
+  )
+} 
+
+export default App
